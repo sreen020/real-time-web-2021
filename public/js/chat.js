d296d33492f6e49703e2ee724838eac77dc0b185
sreen020 <sjoerdreen@gmail.com>
2021-04-19 14:15:23 +0200

    added DB connection and remove chat

--- public/js/chat.js
+++ public/js/chat.js
@@ -21,6 +21,12 @@ if (button) {
         message: message.value,
         handle: handle.value,
       });
+
+      firebase.database().ref("message").push().set({
+        sender: handle.value,
+        message: message.value,
+      });
+
       message.value = "";
     }
 
@@ -30,11 +36,41 @@ if (button) {
   });
 }
 
-socket.on("chatMessage", (data) => {
-  feedback.innerHTML = "";
-  output.innerHTML +=
-    "<p><strong>" + data.handle + ": </strong>" + data.message + "</p>";
-});
+// socket.on("chatMessage", (data) => {
+//   feedback.innerHTML = "";
+//   output.innerHTML +=
+//     "<p><strong>" + data.handle + ": </strong>" + data.message + "</p>";
+// });
+
+firebase
+  .database()
+  .ref("message")
+  .on("child_added", (snapshot) => {
+    feedback.innerHTML = "";
+    output.innerHTML +=
+      "<div class=" +
+      "message-container" +
+      "  id=" +
+      "message-" +
+      snapshot.key +
+      "><p><strong>" +
+      snapshot.val().sender +
+      ": </strong>" +
+      snapshot.val().message;
+    +"</p></div>";
+
+    if (handle.value == snapshot.val().sender) {
+      const messageContainer = document.getElementById(
+        "message-" + snapshot.key
+      );
+      messageContainer.innerHTML +=
+        "<button data-id=" +
+        snapshot.key +
+        " onclick=" +
+        "deleteMessage(this)" +
+        ">D</button>";
+    }
+  });
 
 if (message) {
   message.addEventListener("keypress", () => {
@@ -45,3 +81,26 @@ if (message) {
 socket.on("typing", (data) => {
   feedback.innerHTML = "<p><em>" + data + ": is typing a message </em></p>";
 });
+
+function deleteMessage(self) {
+  const messageId = self.getAttribute("data-id");
+  console.log(messageId);
+
+  firebase.database().ref("message").child(messageId).remove();
+}
+
+firebase
+  .database()
+  .ref("message")
+  .on("child_removed", (snapshot) => {
+    // const test = document.getElementById("message-" + snapshot.key + " p").innerHTML =
+    //   "this message has been removed";
+
+    document.querySelector("#message-" + snapshot.key + " p").innerHTML =
+      "this message has been removed";
+
+    const button = document.querySelector(
+      "#message-" + snapshot.key + " button"
+    );
+    button.parentNode.removeChild(button);
+  });
---
+++ public/js/storeChat.js
@@ -0,0 +1,11 @@
+// const msg = document.getElementById("message").value;
+// const personName = document.getElementById("handle").value;
+
+// button.addEventListener("click", (e) => {
+//   e.preventDefault();
+
+//   firebase.database().ref("message").push().set({
+//     sender: personName,
+//     message: msg,
+//   });
+// });
